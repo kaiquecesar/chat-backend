2aef0d6c583bdb133ed431855ba39ec9ac967a45
Kaique Cesar da Silva <159555005+kaiquecesar@users.noreply.github.com>
2025-10-08 08:14:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // 🔹 Conexão com seu banco PostgreSQL no Render
 const pool = new Pool({
   connectionString:
-    "postgresql://chat_db_93e6_user:xWGz5hB1P2PJFaZvrcgzzvg1tt3HSAsH@dpg-d3j1vvt6ubrc73a06240-a.oregon-postgres.render.com/chat_db_93e6",
+    "postgresql://chat_db_5pd2_user:uBIfeynxQLJbGK5BKUeiPNdf1GjN9n9G@dpg-d3j4ce9r0fns73ajv9s0-a/chat_db_5pd2",
   ssl: { rejectUnauthorized: false },
 });
 
